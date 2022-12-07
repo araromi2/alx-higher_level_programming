5ea9a80f071ce5425e867db503cb8660603b50f0
araromi2 <abdulhameedararomi@gmail.com>
2022-12-06 17:30:44 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,28 +1,79 @@
 #include "lists.h"
-
-int is_palindrome(listint_t **head)
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: pointer to the head of the list
+ * Return: number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
 {
-	listint_t *current = *head;
-	int array[1000];
-	int i = 0, j = 0;
+	size_t i = 0;
 
-	if (current == NULL)
-		return (1);
-	while (current)
+	while (h)
 	{
-		array[i] = current->n;
-		current = current->next;
+		h = h->next;
 		i++;
 	}
-	i--;
+	return (i);
+}
 
-	while (i > j)
+/**
+ * find_middle - finds the middle of a linked list
+ * @head: pointer to the head of the list
+ * Return: pointer to the middle of the list
+ */
+listint_t *find_middle(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (fast && fast->next)
 	{
-		if (array[i] != array[j])
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (slow);
+}
+
+/**
+ * reverse_list - reverses a linked list
+ * @head: pointer to the head of the list
+ * Return: pointer to the head of the reversed list
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double pointer to the head of the list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *middle, *start = *head;
+	size_t len, i;
+
+	if (!head || !*head)
+		return (1);
+	len = listint_len(*head);
+	middle = find_middle(*head);
+	middle = reverse_list(&middle);
+	for (i = 0; i < len / 2; i++)
+	{
+		if (start->n != middle->n)
 			return (0);
-		i--;
-		j++;
+		start = start->next;
+		middle = middle->next;
 	}
 	return (1);
 }
-
