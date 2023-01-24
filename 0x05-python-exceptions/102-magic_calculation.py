4e41f7a0916c4e664193e99b3e6bf11f149c9ac6
araromi2 <abdulhameedararomi@gmail.com>
2023-01-24 06:31:02 -0800

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except Exception as e:
+            result = b + a
+    return result
