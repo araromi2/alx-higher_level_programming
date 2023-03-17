abc3d1e2101db878a028c61d97f8a9161f42838b
abdulhameed <abdulhameedararomi@gmail.com>
2023-03-17 13:19:35 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,14 +8,14 @@ class Rectangle {
   }
 
   print () {
+    let arr = [];
     for (let i = 0; i < this.height; i++) {
-      let s = '';
       for (let j = 0; j < this.width; j++) {
-        s += 'X';
+        arr += 'X';
       }
-      console.log(s);
+      console.log(arr);
+      arr = [];
     }
   }
 }
-
 module.exports = Rectangle;
