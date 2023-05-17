1e80285f2840ab30508440f9db75a858a7fe0e4a
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-16 17:38:57 -0700

    Add a script that list the number of records with a condition passed in the table

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- lists the number of records with the same score in the table second_table
+-- The database name will be passed as an argument of the mysql command
 SELECT score, count(*) AS number
 FROM second_table
 GROUP BY score
