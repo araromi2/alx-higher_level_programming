ec51fa2943a2e511fc7d2c999b8f619762999eb5
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-16 17:41:39 -0700

    Add a script that lists all records of a table that is not null

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,6 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
+-- don't list rows without a name value
+-- The database name will be passed as an argument of the mysql command
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
