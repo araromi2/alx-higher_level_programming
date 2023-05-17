6e4c92da979cb68fd6faf0fc77418109832262bb
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-16 17:27:17 -0700

    Add a script that lists all records of a table displaying certain column

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
 -- list all records of the table second_table
+-- Ordered by score
+-- The database name will be passed as an argument of the mysql command
 SELECT `score`, `name` FROM second_table
 ORDER BY score DESC;
