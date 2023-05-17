3daaa867cc0edaa060b9f02e88195dbe812220bf
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-16 17:30:48 -0700

    Add a script that lists all record within a threshold in a table

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,6 @@
 -- lists all records with a score >= 10 in the table second_table
+-- ordered by score
+-- The database name will be passed as an argument of the mysql command
 SELECT score, name FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
