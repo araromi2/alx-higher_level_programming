ddfa5429755a01c19a048984cc9ebfaeeaa2ff03
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-16 17:34:05 -0700

    Add a script that updates a field in a table

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,6 @@
 -- updates the score of Bob to 10 in the table second_table
+-- only ise the name field
+-- The database name will be passed as an argument of the mysql command
 UPDATE second_table
 SET score = 10
 WHERE name = "Bob";
