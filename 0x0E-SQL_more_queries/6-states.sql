dd8a2901cb56d18be589924000aff030bd39ecfc
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-18 05:52:47 -0700

    Update script by adding a semicolon after the first command

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creats hbtn_0d_usa database and states table with id and name description
 CREATE DATABASE
-	IF NOT EXISTS `hbtn_0d_usa`
+	IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE
 	IF NOT EXISTS `hbtn_0d_usa`.`states` (
 		PRIMARY KEY(`id`),
