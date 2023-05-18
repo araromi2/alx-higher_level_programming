b970677ba45847ccf321e00b6ddeb221eaaa0be1
abdulhameed <abdulhameedararomi@gmail.com>
2023-05-18 06:06:22 -0700

    Add a script that demonstrate subqueries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- lists all cities of California that can be found in the hbtn_0d_usa db
+SELECT `id`, `name`
+FROM `cities`
+WHERE `state_id` IN
+	(SELECT `id`
+		FROM `states`
+		WHERE `name` = "California")
+ORDER BY `id`;
